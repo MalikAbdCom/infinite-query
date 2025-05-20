8799e26e3a4a9f87ac84cefd784d6d0181b51799
malikabdcom <emailmalikabdcom@gmail.com>
2025-05-20 13:53:31 +0700

    finished code

--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,13 +1,65 @@
 import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import {Person} from "./Person";
+import {useInfiniteQuery} from "@tanstack/react-query";
 
 const initialUrl = "https://swapi-node.vercel.app/api/people/";
 const fetchUrl = async (url) => {
-  const response = await fetch(url);
-  return response.json();
+    const response = await fetch(url);
+    return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+    // TODO: get data for InfiniteScroll via React Query
+
+    const {
+        data,
+        isLoading,
+        isFetching,
+        error,
+        fetchNextPage,
+        hasNextPage,
+    } = useInfiniteQuery({
+        queryKey: ['sw-people'],
+        queryFn: ({pageParams = initialUrl}) => fetchUrl(pageParams),
+        getNextPageParam: lastPage => lastPage.next || undefined,
+    })
+
+    if (isLoading) {
+        return <h1>loading...</h1>
+    }
+
+    console.log({
+        data
+    })
+
+    return (
+        <>
+            { isFetching && <h1 className={'loading'}>loading...</h1>}
+            <InfiniteScroll
+                loadMore={
+                    () => {
+                        if (!isFetching) {
+                            return fetchNextPage()
+                        }
+                    }
+                }
+                hasMore={hasNextPage}
+            >
+                {data.pages.map(page => {
+                    return (
+                        <>
+                            {
+                                page.results.map(result => {
+                                    return (
+                                        <Person name={result.fields.name} eyeColor={result.fields.eye_color}
+                                                hairColor={result.fields.hair_color} key={result.name}/>
+                                    )
+                                })
+                            }
+                        </>
+                    )
+                })}
+            </InfiniteScroll>
+        </>
+    );
 }
